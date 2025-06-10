f4bc90a791eede3fe9b973ba7a04d173c582eba7
Tagir <sulaev.tagir@mail.ru>
2025-06-10 20:25:16 +0300

    unused files removed

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<router-outlet />`
 })
+
 export class App {
   protected title = 'ndm-test';
 }
